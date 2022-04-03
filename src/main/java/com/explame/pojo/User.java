e236ee6b3c9ad1b74c52f72d831bb11162f83e2d
呵呵 <a473865916@163.com>
2022-04-03 15:55:00 +0800

    111

---
+++ src/main/java/com/explame/pojo/User.java
@@ -0,0 +1,22 @@
+package com.explame.pojo;
+
+public class User {
+    private Integer age;
+    private String uname;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+}
